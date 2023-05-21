0af455298e77a5a230109ac67f33dab73539f35d
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2023-05-21 19:41:35 +0600

    responsive done to 700px

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", function () {
   var nav = document.querySelector("nav");
   nav.classList.toggle("sticky", this.window.scrollY > 0);
 });
+
+const date = new Date();
+document.querySelector(
+  ".footer__span"
+).textContent = `${date.getFullYear()}`;
