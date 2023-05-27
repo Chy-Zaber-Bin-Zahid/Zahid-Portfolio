e6f724c61f782ff2c34d88ca9b7a5488139bf267
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2023-05-27 20:57:07 +0600

    fixed big screne issue

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
+// nav
 window.addEventListener("scroll", function () {
-  var nav = document.querySelector("nav");
+  let nav = document.querySelector("nav");
   nav.classList.toggle("sticky", this.window.scrollY > 0);
 });
 
+// on scroll reveal
+window.addEventListener("scroll", () => {
+  let reveal = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveal.length; i++) {
+    let windowHight = window.innerHeight;
+    let revealTop = reveal[i].getBoundingClientRect().top;
+    let revealPoint = 0;
+
+    if (revealTop < windowHight - revealPoint) {
+      reveal[i].classList.add("active");
+    }
+    
+  }
+});
+
+// footer
 const date = new Date();
-document.querySelector(
-  ".footer__span"
-).textContent = `${date.getFullYear()}`;
+document.querySelector(".footer__span").textContent = `${date.getFullYear()}`;
